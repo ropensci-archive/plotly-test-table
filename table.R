4b9a91ed10aa6ec3e2b7c198ed370c59861ba1fc
Toby Dylan Hocking <tdhock5@gmail.com>
2015-03-02 18:42:09 -0500

    ignore.case=TRUE to support both old and new versions of xtable

--- table.R
+++ table.R
@@ -11,11 +11,11 @@ code_commits <- read.csv("code_commits.csv", as.is=TRUE, comment.char="#")
 ## Parse the first occurance of pattern from each of several strings
 ## using (named) capturing regular expressions, returning a matrix
 ## (with column names).
-str_match_perl <- function(string,pattern){
+str_match_perl <- function(string,pattern,...){
   stopifnot(is.character(string))
   stopifnot(is.character(pattern))
   stopifnot(length(pattern)==1)
-  parsed <- regexpr(pattern,string,perl=TRUE)
+  parsed <- regexpr(pattern,string,perl=TRUE,...)
   captured.text <- substr(string,parsed,parsed+attr(parsed,"match.length")-1)
   captured.text[captured.text==""] <- NA
   captured.groups <- if(is.null(attr(parsed, "capture.start"))){
@@ -35,11 +35,11 @@ str_match_perl <- function(string,pattern){
 ## Parse several occurances of pattern from each of several strings
 ## using (named) capturing regular expressions, returning a list of
 ## matrices (with column names).
-str_match_all_perl <- function(string,pattern){
+str_match_all_perl <- function(string,pattern,...){
   stopifnot(is.character(string))
   stopifnot(is.character(pattern))
   stopifnot(length(pattern)==1)
-  parsed <- gregexpr(pattern,string,perl=TRUE)
+  parsed <- gregexpr(pattern,string,perl=TRUE, ...)
   lapply(seq_along(parsed),function(i){
     r <- parsed[[i]]
     full <- substring(string[i],r,r+attr(r,"match.length")-1)
@@ -337,8 +337,10 @@ th.pattern <-
 column.labels <- c()
 for(index.file in index){
   ilines <- readLines(index.file)
-  th.line <- grep("<TH>", ilines, value=TRUE)
-  label.mat <- str_match_all_perl(th.line, th.pattern)[[1]]
+  th.line <- grep("<TH>", ilines, value=TRUE, ignore.case = TRUE)
+  stopifnot(length(th.line) == 1)
+  label.mat <-
+    str_match_all_perl(th.line, th.pattern, ignore.case=TRUE)[[1]]
   label <- label.mat[-1, "label"]
   label <- label[label != "ggplot2"]
   column.labels[[index.file]] <- paste(label, collapse = "<br />")
